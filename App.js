f3f50ae596c3858340e75d84bc639dfcca6ce9a7
Michael Ma <mamichael326@gmail.com>
2020-09-03 21:30:51 -0700

    showing inital map and location

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Constants } from "expo";
+import * as Location from "expo-location";
+import MapView from "react-native-maps";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+	state = {
+		mapRegion: {
+			latitude: 37.78825,
+			longitude: -122.4324,
+			latitudeDelta: 0.005,
+			longitudeDelta: 0.005,
+		},
+		hasLocationPermissions: false,
+		locationResult: null,
+	};
+
+	componentDidMount() {
+		this._getLocationAsync();
+	}
+
+	_handleMapRegionChange = (mapRegion) => {
+		console.log(mapRegion);
+	};
+
+	_getLocationAsync = async () => {
+		let { status } = await Location.requestPermissionsAsync();
+		if (status !== "granted") {
+			setErrorMsg("Permission to access location was denied");
+		}
+
+		let location = await Location.getCurrentPositionAsync({});
+		this.setState({
+			mapRegion: {
+				...this.state.mapRegion,
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+			},
+		});
+		console.log(this.state.mapRegion);
+	};
+
+	render() {
+		return (
+			<MapView
+				style={styles.map}
+				region={this.state.mapRegion}
+				onRegionChange={this._handleMapRegionChange}
+			></MapView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#ecf0f1",
+	},
+	paragraph: {
+		margin: 24,
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center",
+		color: "#34495e",
+	},
+	map: {
+		flex: 1,
+	},
 });

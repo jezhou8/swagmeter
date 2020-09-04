3938ca7af74a3ea3a45c6ab754b27ede1c2d8e43
Michael Ma <mamichael326@gmail.com>
2020-09-03 22:13:55 -0700

    added hwid

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { Constants } from "expo";
 import * as Location from "expo-location";
 import MapView from "react-native-maps";
+import Constants from "expo-constants";
 
 export default class App extends Component {
 	state = {
 		mapRegion: {
-			latitude: 37.78825,
-			longitude: -122.4324,
+			latitude: 40.103841,
+			longitude: -88.227274,
 			latitudeDelta: 0.005,
 			longitudeDelta: 0.005,
 		},
 		hasLocationPermissions: false,
 		locationResult: null,
+		deviceID: Constants.installationId,
 	};
 
 	componentDidMount() {
 		this._getLocationAsync();
 	}
 
-	_handleMapRegionChange = (mapRegion) => {
-		console.log(mapRegion);
-	};
+	_handleMapRegionChange = (mapRegion) => {};
 
 	_getLocationAsync = async () => {
 		let { status } = await Location.requestPermissionsAsync();

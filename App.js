bb32232bfe417e7218004f8f022a198a2f5f7453
Jerry Zhou <jezhou8@gmail.com>
2020-09-04 00:43:42 -0500

    basic firebase

--- App.js
+++ App.js
@@ -1,32 +1,47 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import * as Location from "expo-location";
-import MapView from "react-native-maps";
-import Constants from "expo-constants";
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+import Constants from 'expo-constants';
+import { firestore, firestoreRef } from './firebase/app';
 
 export default class App extends Component {
 	state = {
 		mapRegion: {
 			latitude: 40.103841,
 			longitude: -88.227274,
-			latitudeDelta: 0.005,
-			longitudeDelta: 0.005,
+			latitudeDelta: 0.0005,
+			longitudeDelta: 0.0005,
 		},
-		hasLocationPermissions: false,
-		locationResult: null,
 		deviceID: Constants.installationId,
 	};
+
+	constructor(props) {
+		super(props);
+		this._updateDatabase();
+	}
 
 	componentDidMount() {
 		this._getLocationAsync();
 	}
 
+	_updateDatabase = () => {
+		firestore
+			.collection('users')
+			.doc(this.state.deviceID)
+			.set(
+				{
+					...this.state,
+				},
+				{ merge: true }
+			);
+	};
 	_handleMapRegionChange = (mapRegion) => {};
 
 	_getLocationAsync = async () => {
 		let { status } = await Location.requestPermissionsAsync();
-		if (status !== "granted") {
-			setErrorMsg("Permission to access location was denied");
+		if (status !== 'granted') {
+			setErrorMsg('Permission to access location was denied');
 		}
 
 		let location = await Location.getCurrentPositionAsync({});
@@ -46,6 +61,7 @@ export default class App extends Component {
 				style={styles.map}
 				region={this.state.mapRegion}
 				onRegionChange={this._handleMapRegionChange}
+				showsUserLocation
 			></MapView>
 		);
 	}
@@ -54,16 +70,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: "#ecf0f1",
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#ecf0f1',
 	},
 	paragraph: {
 		margin: 24,
 		fontSize: 18,
-		fontWeight: "bold",
-		textAlign: "center",
-		color: "#34495e",
+		fontWeight: 'bold',
+		textAlign: 'center',
+		color: '#34495e',
 	},
 	map: {
 		flex: 1,
---
+++ firebase/app.js
@@ -0,0 +1,8 @@
+import * as Firebase from 'firebase';
+import 'firebase/firestore';
+import firebaseConfig from './config.js';
+let app = Firebase.initializeApp(firebaseConfig);
+export const firestore = app.firestore();
+export const firebaseAuth = Firebase.auth;
+export const firebaseAuthFunc = app.auth();
+export const firestoreRef = Firebase.firestore;
